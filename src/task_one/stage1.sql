347d29abb68429637732dde78d86746745361f04
raztras <rastra1997@gmail.com>
2026-07-29 12:38:18 +0100

    COMMENTS ADDED

--- src/task_one/stage1.sql
+++ src/task_one/stage1.sql
@@ -1,6 +1,24 @@
+--## Stage 1 - Calculate Daily Reliability
+
+--The database `measurements` table contains telemetry data for a single site at 30-minute intervals.
+
+--### Telemetry
+
+--| Column | Description |
+--| --- | --- |
+--| `timestamp` | Measurement timestamp |
+--| `active_power` | Measured active power (kW) |
+--| `setpoint` | Requested active power (kW) |
+
+--The Site Capacity is given as `100 MW`.
+--
+--### Task
+--
+--Write the SQL to calculate the daily average Active Power Reliability.
+
 select
     date_trunc('day', "timestamp") as day,
     avg(1 - abs(active_power - setpoint) / 100000.0) as avg_reliability
 from measurements
-group by 1
-order by 1;
+group by day
+order by day;
--- src/task_one/stage2.sql
+++ src/task_one/stage2.sql
@@ -1,3 +1,24 @@
+--## Stage 2 - Supporting Multiple Sites
+--The platform has now expanded to support multiple sites.
+--Telemetry now includes a `site_id` field, and site capacities are stored in a separate table:
+
+--`multi_site_measurements`
+--| Column | Description |
+--| --- | --- |
+--| `timestamp` | Measurement timestamp |
+--| `site_id` | Site identifier |
+--| `active_power` | Measured active power (kW) |
+--| `setpoint` | Requested active power (kW) |
+
+--`site_capacities`
+--| Column | Description |
+--| --- | --- |
+--| `site_id` | Site identifier |
+--| `capacity_mw` | Installed site capacity (MW) |
+
+--### Task
+--Write the SQL to calculate the daily average reliabilities for each site.
+
 select
     m.site_id,
     date_trunc('day', m."timestamp") as day,
